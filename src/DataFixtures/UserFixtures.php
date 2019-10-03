81ca92279a9835cd685614d081dedf05c167faf5
Lothindir <lothindir@gmail.com>
2019-10-03 15:54:30 +0200

    Updated Fixtures and populated database
    * Added Faker as a dependency

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,20 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
 
 class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         for ($i = 0; $i < 20; $i++) {
+
+            $faker = Faker\Factory::create('fr_CH');
+
             $user = new User();
-            $user->setName('User'.$i);
-            $user->setEntryDate(new \DateTime());
-            $hashedPwd = password_hash($user->getName().$user->getEntryDate()->getTimestamp(), PASSWORD_BCRYPT);
-            $user->setHashedPassword($hashedPwd);
+            $user->setName($faker->userName);
+            $user->setEntryDate($faker->dateTimeBetween('-15 years'));
+            $user->setHashedPassword($faker->sha256);
             $manager->persist($user);
 
-            $this->addReference($user->getName(), $user);
+            $this->addReference('User'.$i, $user);
         }
 
         $manager->flush();
