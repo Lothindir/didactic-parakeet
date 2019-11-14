88e044dbcb1f7fe252acd2f9f91a4d4bd51e953c
Lothindir <lothindir@gmail.com>
2019-11-14 16:20:43 +0100

    Edited AddBookForm

--- src/Form/AddBookType.php
+++ src/Form/AddBookType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +16,27 @@ class AddBookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Title')
-            ->add('ExtractLink')
-            ->add('Summary')
-            ->add('AuthorLastName')
-            ->add('AuthorFirstName')
-            ->add('Editor')
-            ->add('NumberPages')
+            ->add('Title', TextType::class, [
+                'label' => 'Titre du livre',
+            ])
+            ->add('ExtractLink', UrlType::class, [
+                'label' => 'Lien de l\'extrait'
+            ])
+            ->add('Summary', TextareaType::class, [
+                'label' => 'Résumé'
+            ])
+            ->add('AuthorLastName', TextType::class, [
+                'label' => 'Nom de l\'auteur'
+            ])
+            ->add('AuthorFirstName', TextType::class, [
+                'label' => 'Prénom de l\'auteur'
+            ])
+            ->add('Editor', TextType::class, [
+                'label' => 'Editeur'
+            ])
+            ->add('NumberPages', IntegerType::class, [
+                'label' => 'Nombre de pages'
+            ])
             ->add('ReleaseDate')
             ->add('CoverImage')
             ->add('AddedDate')
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,19 +5,18 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\IsTrue;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Username', null, [
+            ->add('Username', TextType::class, [
                 'label' => 'Nom d\'utilisateur',
             ])
             ->add('plainPassword', PasswordType::class, [
