0c4912b609f3ffc2758a2c490ad3fa9e33dc9712
Lothindir <lothindir@gmail.com>
2020-01-07 15:07:15 +0100

    feat: Rewrote htaccess to redirect to public/index.php
    fix: Changed links to assets to public/...

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
-    .setPublicPath('public/build')
+    .setPublicPath('/public/build')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    .setManifestKeyPrefix('public/')
 
     .copyFiles({
         from: './assets/images',
